09c2f46d53302710bb84803da0d6d55d619e3d2c
esteban vargas <esteban.var@outlook.com>
2022-11-08 20:00:34 -0300

    add index js file

---
+++ index.js
@@ -0,0 +1,28 @@
+let fs = require("fs")
+
+
+class Contenedor{
+    constructor(file){
+        this.file = file
+    }
+
+
+
+    save(){
+
+    }
+    getById(){
+
+    }
+    getAll(){
+
+    }
+    deleteById(){
+
+    }
+    deleteAll(){
+
+    }
+}
+
+const file = new ('/products.txt')
